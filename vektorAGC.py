6533d2d5f4bcb6ef4e8b984d6a8c1b6a643b06d5
FelixS25R <feli14871@gmail.com>
2026-05-29 11:18:48 +0200

    tilføjet så man kan vælge om man vil minus eller plus

--- vektorAGC.py
+++ vektorAGC.py
@@ -1,14 +1,21 @@
 import vektor
 
-x = int(input("Indtast et tal: "))  
-y = int(input("Indtast et andet tal: "))
+typeOperation = int(input("Indtast 1 for plus, 2 for minus, 3 for skalar: "))
 
-resultatPlus = vektor.regnplus(x, y)
-print(resultatPlus)  # Output: x+y
+if typeOperation == 1:
+    x = int(input("Indtast et tal: "))  
+    y = int(input("Indtast et andet tal: "))
 
+    resultatPlus = vektor.regnplus(x, y)
+    print(resultatPlus)  # Output: x+y
+elif typeOperation == 2:
+    x = int(input("Indtast et tal: "))  
+    y = int(input("Indtast et andet tal: "))
 
-resultatMinus = vektor.regnminus(x, y)
-print(resultatMinus)  # Output: x-y
-
-#resultatSkalar = vektor.regnskalar(x, y)
-#print(resultatSkalar)  # Output: x*y
+    resultatMinus = vektor.regnminus(x, y)
+    print(resultatMinus)  # Output: x-y
+#elif typeOperation == 3:
+    #x = int(input("Indtast et tal: "))  
+    #y = int(input("Indtast din skalar: "))
+    #resultatSkalar = vektor.regnskalar(x, y)
+    #print(resultatSkalar)  # Output: x*y
